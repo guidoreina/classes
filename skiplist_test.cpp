0c9faaa0f920d3b40d70c7c20d25047dfc39f050
Guido Reina <guido.reina@yahoo.com>
2013-07-04 19:19:12 +0200

    Added concurrent skip list.

--- skiplist_test.cpp
+++ skiplist_test.cpp
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sched.h>
 #include "util/skiplist.h"
+#include "util/concurrent/skiplist.h"
+#include "util/concurrent/locks/spinlock.h"
 
+static const unsigned kNumberReaders = 33;
+static const unsigned kNumberWriters = 33;
+static const unsigned kNumberErasers = 33;
+static const unsigned kNumberIterators = 33;
 static const unsigned kHighestRandom = 64 * 1024;
 
 struct longcmp {
@@ -20,14 +29,414 @@ enum state {
 };
 
 static state numbers[kHighestRandom];
+static bool running = true;
+
+static util::concurrent::locks::spinlock lock;
+
+static void test_concurrent_skiplist();
+static void* reader(void* arg);
+static void* writer(void* arg);
+static void* eraser(void* arg);
+static void* iterator(void* arg);
+
+static void test_concurrent_skiplist_iterators();
+static void print_list(const util::concurrent::skiplist<long, longcmp>& list, bool forward);
 
 static void test_single_threaded_skiplist();
 static void print_list(const util::skiplist<long, longcmp>& list, bool forward);
 
 int main(int argc, char** argv)
 {
-	test_single_threaded_skiplist();
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <test-number>\n", argv[0]);
+		fprintf(stderr, "\t0: Test concurrent skip list with:\n");
+		fprintf(stderr, "\t\t%u readers\n", kNumberReaders);
+		fprintf(stderr, "\t\t%u writers, %u\n", kNumberWriters);
+		fprintf(stderr, "\t\t%u erasers\n", kNumberErasers);
+		fprintf(stderr, "\t\t%u iterators.\n", kNumberIterators);
+		fprintf(stderr, "\t1: Test iterators in concurrent skip list.\n");
+		fprintf(stderr, "\t2: Test skip list (one thread).\n");
+
+		return -1;
+	}
+
+	switch (atoi(argv[1])) {
+		case 0:
+			test_concurrent_skiplist();
+			break;
+		case 1:
+			test_concurrent_skiplist_iterators();
+			break;
+		case 2:
+			test_single_threaded_skiplist();
+			break;
+		default:
+			fprintf(stderr, "Wrong test number %s.\n", argv[1]);
+			return -1;
+	}
+
 	return 0;
+}
+
+void test_concurrent_skiplist()
+{
+	util::concurrent::skiplist<long, longcmp> list;
+
+	// Initialize list.
+	if (!list.init()) {
+		fprintf(stderr, "Couldn't initialize skip list.\n");
+		return;
+	}
+
+	// Block signals SIGINT and SIGTERM.
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigaddset(&set, SIGTERM);
+	if (pthread_sigmask(SIG_BLOCK, &set, NULL) != 0) {
+		fprintf(stderr, "Error blocking signals.\n");
+		return;
+	}
+
+	for (unsigned i = 0; i < kHighestRandom; i++) {
+		numbers[i] = NUMBER_NOT_USED;
+	}
+
+	// Create readers.
+	pthread_t readers[kNumberReaders];
+	for (unsigned i = 0; i < kNumberReaders; i++) {
+		if (pthread_create(&readers[i], NULL, reader, &list) != 0) {
+			fprintf(stderr, "Couldn't create reader %u.\n", i);
+			return;
+		}
+	}
+
+	// Create writers.
+	pthread_t writers[kNumberWriters];
+	for (unsigned i = 0; i < kNumberWriters; i++) {
+		if (pthread_create(&writers[i], NULL, writer, &list) != 0) {
+			fprintf(stderr, "Couldn't create writer %u.\n", i);
+			return;
+		}
+	}
+
+	// Create erasers.
+	pthread_t erasers[kNumberErasers];
+	for (unsigned i = 0; i < kNumberErasers; i++) {
+		if (pthread_create(&erasers[i], NULL, eraser, &list) != 0) {
+			fprintf(stderr, "Couldn't create eraser %u.\n", i);
+			return;
+		}
+	}
+
+	// Create iterators.
+	pthread_t iterators[kNumberIterators];
+	for (unsigned i = 0; i < kNumberIterators; i++) {
+		if (pthread_create(&iterators[i], NULL, iterator, &list) != 0) {
+			fprintf(stderr, "Couldn't create iterator %u.\n", i);
+			return;
+		}
+	}
+
+	printf("Send SIGINT (Control + C) or SIGTERM to finish the test...\n");
+
+	// Wait for signal to arrive.
+	int sig;
+	while (sigwait(&set, &sig) != 0);
+
+	printf("Signal received.\n");
+
+	running = false;
+
+	// Wait for readers.
+	for (unsigned i = 0; i < kNumberReaders; i++) {
+		pthread_join(readers[i], NULL);
+	}
+
+	// Wait for writers.
+	for (unsigned i = 0; i < kNumberWriters; i++) {
+		pthread_join(writers[i], NULL);
+	}
+
+	// Wait for erasers.
+	for (unsigned i = 0; i < kNumberErasers; i++) {
+		pthread_join(erasers[i], NULL);
+	}
+
+	// Wait for iterators.
+	for (unsigned i = 0; i < kNumberIterators; i++) {
+		pthread_join(iterators[i], NULL);
+	}
+}
+
+void* reader(void* arg)
+{
+	util::concurrent::skiplist<long, longcmp> *list = reinterpret_cast<util::concurrent::skiplist<long, longcmp>*>(arg);
+
+	pthread_t thread = pthread_self();
+
+	struct drand48_data data;
+	srand48_r(0xc0c0c0c0, &data);
+
+	do {
+		long n;
+		state state;
+
+		do {
+			if (!running) {
+				return 0;
+			}
+
+			lrand48_r(&data, &n);
+			n %= kHighestRandom;
+
+			lock.lock();
+
+			state = numbers[n];
+			if ((state == NUMBER_INSERTED) || (state == NUMBER_ERASED)) {
+				numbers[n] = NUMBER_IN_USE;
+				lock.unlock();
+
+				break;
+			}
+
+			lock.unlock();
+		} while (true);
+
+		if (state == NUMBER_INSERTED) {
+			if (!list->contains(n)) {
+				fprintf(stderr, "[%ld] %u not found.\n", thread, n);
+			}
+		} else {
+			if (list->contains(n)) {
+				fprintf(stderr, "[%ld] %u found.\n", thread, n);
+			}
+		}
+
+		lock.lock();
+		numbers[n] = state;
+		lock.unlock();
+	} while (true);
+
+	return NULL;
+}
+
+void* writer(void* arg)
+{
+	util::concurrent::skiplist<long, longcmp> *list = reinterpret_cast<util::concurrent::skiplist<long, longcmp>*>(arg);
+
+	pthread_t thread = pthread_self();
+
+	struct drand48_data data;
+	srand48_r(0xc0c0c0c0, &data);
+
+	do {
+		long n;
+		state state;
+
+		do {
+			if (!running) {
+				return 0;
+			}
+
+			lrand48_r(&data, &n);
+			n %= kHighestRandom;
+
+			lock.lock();
+
+			state = numbers[n];
+			if ((state == NUMBER_NOT_USED) || (state == NUMBER_ERASED)) {
+				numbers[n] = NUMBER_IN_USE;
+				lock.unlock();
+
+				break;
+			}
+
+			lock.unlock();
+		} while (true);
+
+		if (!list->insert(n)) {
+			fprintf(stderr, "[%ld] Couldn't insert %u.\n", thread, n);
+		}
+
+		lock.lock();
+		numbers[n] = NUMBER_INSERTED;
+		lock.unlock();
+	} while (true);
+
+	return NULL;
+}
+
+void* eraser(void* arg)
+{
+	util::concurrent::skiplist<long, longcmp> *list = reinterpret_cast<util::concurrent::skiplist<long, longcmp>*>(arg);
+
+	pthread_t thread = pthread_self();
+
+	struct drand48_data data;
+	srand48_r(0xc0c0c0c0, &data);
+
+	do {
+		long n;
+		state state;
+
+		do {
+			if (!running) {
+				return 0;
+			}
+
+			lrand48_r(&data, &n);
+			n %= kHighestRandom;
+
+			lock.lock();
+
+			state = numbers[n];
+			if (state == NUMBER_INSERTED) {
+				numbers[n] = NUMBER_IN_USE;
+				lock.unlock();
+
+				break;
+			}
+
+			lock.unlock();
+		} while (true);
+
+		if (!list->erase(n)) {
+			fprintf(stderr, "[%ld] Couldn't erase %u.\n", thread, n);
+		}
+
+		lock.lock();
+		numbers[n] = NUMBER_ERASED;
+		lock.unlock();
+	} while (true);
+
+	return NULL;
+}
+
+void* iterator(void* arg)
+{
+	util::concurrent::skiplist<long, longcmp> *list = reinterpret_cast<util::concurrent::skiplist<long, longcmp>*>(arg);
+
+	util::concurrent::skiplist<long, longcmp>::iterator it;
+
+	bool forward = true;
+
+	do {
+		if (forward) {
+			if (list->begin(it)) {
+				while ((running) && (list->next(it)));
+			}
+		} else {
+			if (list->end(it)) {
+				while ((running) && (list->previous(it)));
+			}
+		}
+
+		forward = !forward;
+
+		sched_yield();
+	} while (running);
+
+	return NULL;
+}
+
+void test_concurrent_skiplist_iterators()
+{
+	util::concurrent::skiplist<long, longcmp> list;
+
+	if (!list.init()) {
+		fprintf(stderr, "Couldn't initialize skip list.\n");
+		return;
+	}
+
+	for (unsigned i = 0; i < kHighestRandom; i++) {
+		numbers[i] = NUMBER_NOT_USED;
+	}
+
+	for (unsigned i = 0; i < 1000; i++) {
+		for (unsigned j = 0; j < 1000; j++) {
+			long n;
+			do {
+				n = random() % kHighestRandom;
+			} while (numbers[n] != NUMBER_NOT_USED);
+
+			if (!list.insert(n)) {
+				fprintf(stderr, "Couldn't insert %d.\n", n);
+				return;
+			}
+
+			numbers[n] = NUMBER_INSERTED;
+		}
+
+		for (unsigned j = 0; j < kHighestRandom; j++) {
+			if (numbers[j] == NUMBER_INSERTED) {
+				if (!list.erase(j)) {
+					fprintf(stderr, "Couldn't erase %d.\n", j);
+					return;
+				}
+
+				numbers[j] = NUMBER_NOT_USED;
+			}
+		}
+	}
+
+	printf("Inserting from 1 to 10...\n");
+	for (long i = 10; i > 0; i--) {
+		if (!list.insert(i)) {
+			fprintf(stderr, "Couldn't insert %d.\n", i);
+			return;
+		}
+	}
+
+	print_list(list, true);
+	print_list(list, false);
+
+	printf("From 5 forward...\n");
+	util::concurrent::skiplist<long, longcmp>::iterator it;
+	if (list.seek(5, it)) {
+		do {
+			printf("%d\n", it.key());
+		} while (list.next(it));
+	}
+
+	printf("Erasing 5...\n");
+	list.erase(5);
+
+	print_list(list, true);
+	print_list(list, false);
+
+	printf("Erasing 1...\n");
+	list.erase(1);
+
+	print_list(list, true);
+	print_list(list, false);
+
+	printf("Erasing 10...\n");
+	list.erase(10);
+
+	print_list(list, true);
+	print_list(list, false);
+}
+
+void print_list(const util::concurrent::skiplist<long, longcmp>& list, bool forward)
+{
+	util::concurrent::skiplist<long, longcmp>::iterator it;
+
+	if (forward) {
+		printf("Forward...\n");
+		if (list.begin(it)) {
+			do {
+				printf("%d\n", it.key());
+			} while (list.next(it));
+		}
+	} else {
+		printf("Backward...\n");
+		if (list.end(it)) {
+			do {
+				printf("%d\n", it.key());
+			} while (list.previous(it));
+		}
+	}
 }
 
 void test_single_threaded_skiplist()
---
+++ util/concurrent/skiplist.h
@@ -0,0 +1,694 @@
+#ifndef UTIL_CONCURRENT_SKIPLIST_H
+#define UTIL_CONCURRENT_SKIPLIST_H
+
+// Implementation of a concurrent skiplist following the algorithm described
+// in the book: "The Art of Multiprocessor Programming, Revised Print"
+// http://www.amazon.com/books/dp/0123973376
+// and in the algorithm described in: "Practical lock-freedom"
+// http://www.cl.cam.ac.uk/techreports/UCAM-CL-TR-579.pdf
+//
+// Notes:
+// As nodes cannot be freed directly, they are saved in a list until the
+// list reaches 'kThreshold' nodes. At this point the oldest node will be
+// deleted.
+
+#include <stdlib.h>
+#include <new>
+#include "util/difference.h"
+#include "util/concurrent/atomic/markable_ptr.h"
+#include "util/concurrent/atomic/atomic.h"
+#include "util/concurrent/locks/spinlock.h" // For the list of deleted nodes.
+
+// Thread Local Storage.
+static __thread struct drand48_data random_data = {{0xc0}};
+
+namespace util {
+	namespace concurrent {
+		template<typename _Key, typename _Compare = util::difference<_Key> >
+		class skiplist {
+			public:
+				// Constructor.
+				skiplist();
+				skiplist(const _Compare& cmp);
+
+				// Destructor.
+				~skiplist();
+
+				// Initialize.
+				bool init();
+
+				// Insert.
+				bool insert(const _Key& k);
+
+				// Erase.
+				bool erase(const _Key& k);
+
+				// Contains.
+				bool contains(const _Key& k) const;
+
+				class iterator {
+					friend class skiplist;
+
+					public:
+						const _Key& key() const;
+
+					private:
+						_Key k;
+				};
+
+				// Begin.
+				bool begin(iterator& it) const;
+
+				// End.
+				bool end(iterator& it) const;
+
+				// Previous.
+				bool previous(iterator& it) const;
+
+				// Next.
+				bool next(iterator& it) const;
+
+				// Seek.
+				bool seek(const _Key& k, iterator& it) const;
+
+			private:
+				// [Probability] p = 0.25
+				// [Maximum number of elements] N = 4294967296
+				// L(n) = log1/p(n)
+				// kMaxLevel = L(N) = log4(4294967296) = 16
+				static const int kMaxLevel = 16;
+
+				struct node {
+					// Key.
+					_Key key;
+
+					// Level.
+					int level;
+
+					// Next deleted node.
+					node* next_deleted;
+
+					// Previous deleted node.
+					node* prev_deleted;
+
+					concurrent::atomic::markable_ptr<node> next[1];
+
+					// Constructor.
+					node(int height);
+					node(const _Key& k, int height);
+				};
+
+				node* _M_header;
+
+				int _M_level_hint;
+
+				_Compare _M_compare;
+
+				struct nodelist {
+					static const int kThreshold = 32 * 1024;
+
+					node* head;
+					node* tail;
+
+					int count;
+
+					concurrent::locks::spinlock lock;
+
+					// Insert.
+					void insert(node* n);
+				};
+
+				nodelist _M_free_nodes;
+
+				// Find.
+				bool find(const _Key& k, node** preds, node** succs);
+				bool find(const _Key& k, const node** preds, const node** succs) const;
+
+				// Make node.
+				node* make_node(int height);
+				node* make_node(const _Key& k, int height);
+
+				// Allocate node.
+				node* allocate_node(int height);
+
+				// Delete node.
+				void delete_node(node* n);
+
+				// Random level.
+				int random_level();
+		};
+
+		template<typename _Key, typename _Compare>
+		inline skiplist<_Key, _Compare>::skiplist()
+		: _M_header(NULL),
+		_M_level_hint(1),
+		_M_compare()
+		{
+			_M_free_nodes.head = NULL;
+			_M_free_nodes.tail = NULL;
+			_M_free_nodes.count = 0;
+		}
+
+		template<typename _Key, typename _Compare>
+		inline skiplist<_Key, _Compare>::skiplist(const _Compare& cmp)
+		: _M_header(NULL),
+		_M_level_hint(1),
+		_M_compare(cmp)
+		{
+			_M_free_nodes.head = NULL;
+			_M_free_nodes.tail = NULL;
+			_M_free_nodes.count = 0;
+		}
+
+		template<typename _Key, typename _Compare>
+		skiplist<_Key, _Compare>::~skiplist()
+		{
+			node* n = _M_header;
+			while (n) {
+				node* next = n->next[0].get();
+				delete_node(n);
+				n = next;
+			}
+
+			n = _M_free_nodes.head;
+			while (n) {
+				node* next = n->next_deleted;
+				delete_node(n);
+				n = next;
+			}
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::init()
+		{
+			// Create header.
+			if ((_M_header = make_node(kMaxLevel)) == NULL) {
+				return false;
+			}
+
+			// Initialize forward pointers to NULL.
+			for (int i = 0; i < kMaxLevel; i++) {
+				_M_header->next[i] = NULL;
+			}
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::insert(const _Key& k)
+		{
+			int level = random_level();
+
+			node* preds[kMaxLevel];
+			node* succs[kMaxLevel];
+
+			node* new_node = NULL;
+
+			do {
+				if (find(k, preds, succs)) {
+					// Already inserted.
+					return false;
+				}
+
+				// Create node.
+				if ((!new_node) && ((new_node = make_node(k, level)) == NULL)) {
+					return false;
+				}
+
+				// Set successor pointers.
+				for (int i = 0; i < level; i++) {
+					new_node->next[i] = succs[i];
+				}
+
+				// Node is considered inserted when it is linked at level 0.
+				if (preds[0]->next[0].compare_and_swap(succs[0], new_node, false, false)) {
+					break;
+				}
+			} while (true);
+
+			for (int i = 1; i < level; i++) {
+				do {
+					// If 'new_node' has been marked as deleted...
+					concurrent::atomic::markable_ptr<node> next = new_node->next[i];
+					if (next.marked()) {
+						find(k, preds, succs);
+						return true;
+					}
+
+					if (next.get() != succs[i]) {
+						bool oldmark;
+						if ((!new_node->next[i].compare_and_swap(next.get(), succs[i], false, false, oldmark)) && (oldmark)) {
+							// 'new_node' has been marked as deleted...
+							find(k, preds, succs);
+							return true;
+						}
+					}
+
+					if (preds[i]->next[i].compare_and_swap(succs[i], new_node, false, false)) {
+						break;
+					}
+
+					find(k, preds, succs);
+				} while (true);
+			}
+
+			// If 'new_node' has been marked as deleted...
+			if (new_node->next[level - 1].marked()) {
+				find(k, preds, succs);
+			}
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::erase(const _Key& k)
+		{
+			node* preds[kMaxLevel];
+			node* succs[kMaxLevel];
+			if (!find(k, preds, succs)) {
+				// Not found.
+				return false;
+			}
+
+			node* node_to_delete = succs[0];
+
+			for (int level = node_to_delete->level - 1; level >= 0; level--) {
+				do {
+					bool oldmark;
+					node* succ = node_to_delete->next[level].get();
+
+					// Try to mark the node as deleted.
+					if (!node_to_delete->next[level].compare_and_swap(succ, succ, false, true, oldmark)) {
+						// If the node has been already marked as deleted...
+						if (oldmark) {
+							if (level == 0) {
+								// Another thread has just deleted the same node.
+								return false;
+							}
+
+							// Some other thread has marked the node as deleted.
+							break;
+						}
+					} else {
+						// Node has been marked as deleted.
+						break;
+					}
+				} while (true);
+			}
+
+			find(k, preds, succs);
+
+			// Add node to the list of free nodes.
+			_M_free_nodes.insert(node_to_delete);
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::contains(const _Key& k) const
+		{
+			const node* pred = _M_header;
+			for (int level = _M_level_hint - 1; level >= 0; level--) {
+				bool marked;
+				const node* curr = pred->next[level].get(marked);
+
+				// If 'pred' has just been marked as deleted...
+				if (marked) {
+					return contains(k);
+				}
+
+				while (curr) {
+					// While 'curr' has been marked as deleted...
+					const node* succ = curr->next[level].get(marked);
+					while (marked) {
+						if ((curr = succ) == NULL) {
+							break;
+						}
+
+						succ = curr->next[level].get(marked);
+					}
+
+					if (curr) {
+						int ret;
+						if ((ret = _M_compare(curr->key, k)) < 0) {
+							pred = curr;
+							curr = succ;
+						} else if (ret == 0) {
+							return true;
+						} else {
+							break;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		template<typename _Key, typename _Compare>
+		inline const _Key& skiplist<_Key, _Compare>::iterator::key() const
+		{
+			return k;
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::begin(iterator& it) const
+		{
+			const node* pred = _M_header;
+			const node* curr;
+			if ((curr = pred->next[0].get()) == NULL) {
+				return false;
+			}
+
+			// While 'curr' has been marked as deleted...
+			bool marked;
+			const node* succ = curr->next[0].get(marked);
+			while (marked) {
+				if ((curr = succ) == NULL) {
+					return false;
+				}
+
+				succ = curr->next[0].get(marked);
+			}
+
+			it.k = curr->key;
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::end(iterator& it) const
+		{
+			const node* pred = _M_header;
+			for (int level = _M_level_hint - 1; level >= 0; level--) {
+				bool marked;
+				const node* curr = pred->next[level].get(marked);
+
+				// If 'pred' has just been marked as deleted...
+				if (marked) {
+					return end(it);
+				}
+
+				while (curr) {
+					// While 'curr' has been marked as deleted...
+					const node* succ = curr->next[level].get(marked);
+					while (marked) {
+						if ((curr = succ) == NULL) {
+							break;
+						}
+
+						succ = curr->next[level].get(marked);
+					}
+
+					if (curr) {
+						pred = curr;
+						curr = succ;
+					} else {
+						break;
+					}
+				}
+			}
+
+			// If the list is empty...
+			if (pred == _M_header) {
+				return false;
+			}
+
+			it.k = pred->key;
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		inline bool skiplist<_Key, _Compare>::previous(iterator& it) const
+		{
+			const node* preds[kMaxLevel];
+			const node* succs[kMaxLevel];
+
+			find(it.k, preds, succs);
+
+			if (preds[0] == _M_header) {
+				return false;
+			}
+
+			it.k = preds[0]->key;
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::next(iterator& it) const
+		{
+			const node* preds[kMaxLevel];
+			const node* succs[kMaxLevel];
+			const node* curr;
+
+			if (!find(it.k, preds, succs)) {
+				if ((curr = succs[0]) == NULL) {
+					return false;
+				}
+			} else {
+				if (((curr = succs[0]) == NULL) || ((curr = curr->next[0].get()) == NULL)) {
+					return false;
+				}
+
+				// While 'curr' has been marked as deleted...
+				bool marked;
+				const node* succ = curr->next[0].get(marked);
+				while (marked) {
+					if ((curr = succ) == NULL) {
+						return false;
+					}
+
+					succ = curr->next[0].get(marked);
+				}
+			}
+
+			it.k = curr->key;
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::seek(const _Key& k, iterator& it) const
+		{
+			const node* preds[kMaxLevel];
+			const node* succs[kMaxLevel];
+			const node* succ;
+
+			find(k, preds, succs);
+
+			if ((succ = succs[0]) == NULL) {
+				return false;
+			}
+
+			it.k = succ->key;
+
+			return true;
+		}
+
+		template<typename _Key, typename _Compare>
+		inline skiplist<_Key, _Compare>::node::node(int height)
+		: level(height)
+		{
+		}
+
+		template<typename _Key, typename _Compare>
+		inline skiplist<_Key, _Compare>::node::node(const _Key& k, int height)
+		: key(k),
+		level(height)
+		{
+		}
+
+		template<typename _Key, typename _Compare>
+		void skiplist<_Key, _Compare>::nodelist::insert(node* n)
+		{
+			lock.lock();
+
+			n->prev_deleted = NULL;
+			n->next_deleted = head;
+
+			if (!tail) {
+				tail = n;
+				count++;
+			} else {
+				head->prev_deleted = n;
+
+				if (count == kThreshold) {
+					node* last = tail;
+					tail = tail->prev_deleted;
+					tail->next_deleted = NULL;
+
+					// Call the destructor.
+					last->~node();
+
+					// Free the memory.
+					free(last);
+				} else {
+					count++;
+				}
+			}
+
+			head = n;
+
+			lock.unlock();
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::find(const _Key& k, node** preds, node** succs)
+		{
+			int ret = -1;
+
+			node* pred = _M_header;
+			for (int level = _M_level_hint - 1; level >= 0; level--) {
+				bool marked;
+				node* curr = pred->next[level].get(marked);
+
+				// If 'pred' has just been marked as deleted...
+				if (marked) {
+					return find(k, preds, succs);
+				}
+
+				while (curr) {
+					// While 'curr' has been marked as deleted...
+					node* succ = curr->next[level].get(marked);
+					while (marked) {
+						if (!pred->next[level].compare_and_swap(curr, succ, false, false)) {
+							// Retry.
+							return find(k, preds, succs);
+						}
+
+						if ((curr = succ) == NULL) {
+							break;
+						}
+
+						succ = curr->next[level].get(marked);
+					}
+
+					if ((curr) && ((ret = _M_compare(curr->key, k)) < 0)) {
+						pred = curr;
+						curr = succ;
+					} else {
+						break;
+					}
+				}
+
+				preds[level] = pred;
+				succs[level] = curr;
+			}
+
+			return (ret == 0);
+		}
+
+		template<typename _Key, typename _Compare>
+		bool skiplist<_Key, _Compare>::find(const _Key& k, const node** preds, const node** succs) const
+		{
+			int ret = -1;
+
+			const node* pred = _M_header;
+			for (int level = _M_level_hint - 1; level >= 0; level--) {
+				bool marked;
+				const node* curr = pred->next[level].get(marked);
+
+				// If 'pred' has just been marked as deleted...
+				if (marked) {
+					return find(k, preds, succs);
+				}
+
+				while (curr) {
+					// While 'curr' has been marked as deleted...
+					const node* succ = curr->next[level].get(marked);
+					while (marked) {
+						if ((curr = succ) == NULL) {
+							break;
+						}
+
+						succ = curr->next[level].get(marked);
+					}
+
+					if ((curr) && ((ret = _M_compare(curr->key, k)) < 0)) {
+						pred = curr;
+						curr = succ;
+					} else {
+						break;
+					}
+				}
+
+				preds[level] = pred;
+				succs[level] = curr;
+			}
+
+			return (ret == 0);
+		}
+
+		template<typename _Key, typename _Compare>
+		inline struct skiplist<_Key, _Compare>::node* skiplist<_Key, _Compare>::make_node(int height)
+		{
+			node* n;
+			if ((n = allocate_node(height)) == NULL) {
+				return NULL;
+			}
+
+			return new (n) node(height);
+		}
+
+		template<typename _Key, typename _Compare>
+		inline struct skiplist<_Key, _Compare>::node* skiplist<_Key, _Compare>::make_node(const _Key& k, int height)
+		{
+			node* n;
+			if ((n = allocate_node(height)) == NULL) {
+				return NULL;
+			}
+
+			return new (n) node(k, height);
+		}
+
+		template<typename _Key, typename _Compare>
+		inline struct skiplist<_Key, _Compare>::node* skiplist<_Key, _Compare>::allocate_node(int height)
+		{
+			return reinterpret_cast<node*>(malloc(sizeof(node) + ((height - 1) * sizeof(concurrent::atomic::markable_ptr<node>))));
+		}
+
+		template<typename _Key, typename _Compare>
+		inline void skiplist<_Key, _Compare>::delete_node(node* n)
+		{
+			// Call the destructor.
+			n->~node();
+
+			// Free the memory.
+			free(n);
+		}
+
+		template<typename _Key, typename _Compare>
+		int skiplist<_Key, _Compare>::random_level()
+		{
+			static const unsigned FRACTION_P = 4;
+
+			int level = 1;
+			long rand;
+
+			do {
+				lrand48_r(&random_data, &rand);
+				if ((rand % FRACTION_P) == 0) {
+					level++;
+				} else {
+					break;
+				}
+			} while (level < kMaxLevel);
+
+			// Increment level hint if required.
+			int level_hint;
+			do {
+				if ((level_hint = _M_level_hint) >= level) {
+					break;
+				}
+			} while (!concurrent::atomic::bool_compare_and_swap(&_M_level_hint, level_hint, level));
+
+			return level;
+		}
+	}
+}
+
+#endif // UTIL_CONCURRENT_SKIPLIST_H
