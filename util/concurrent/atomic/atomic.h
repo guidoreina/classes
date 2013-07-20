e684291b2605eed0f3489e471ef0034726207009
Guido Reina <guido.reina@yahoo.com>
2013-07-20 16:17:14 +0200

    Added atomic functions acquire_load and release_store.

--- util/concurrent/atomic/atomic.h
+++ util/concurrent/atomic/atomic.h
@@ -32,6 +32,32 @@ namespace util {
 			{
 				return __sync_fetch_and_sub(ptr, val);
 			}
+
+			template<typename _T>
+			static inline _T acquire_load(const _T* ptr)
+			{
+#if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7))
+				return __atomic_load_n(ptr, __ATOMIC_ACQUIRE);
+#else
+				__sync_synchronize();
+				_T res = *ptr;
+				__sync_synchronize();
+
+				return res;
+#endif
+			}
+
+			template<typename _T>
+			static inline void release_store(_T* ptr, _T val)
+			{
+#if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7))
+				__atomic_store_n(ptr, val, __ATOMIC_RELEASE);
+#else
+				__sync_synchronize();
+				*ptr = val;
+				__sync_synchronize();
+#endif
+			}
 		}
 	}
 }
